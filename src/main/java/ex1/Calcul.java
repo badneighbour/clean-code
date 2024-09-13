12fc80955b3e78101519bb8caa9063db57a71765
David <david.malvoisin@hotmail.fr>
2024-09-13 13:55:53 +0200

    exercice 1

--- src/main/java/ex1/Calcul.java
+++ src/main/java/ex1/Calcul.java
@@ -1,11 +1,17 @@
 package ex1;
 
-public class calcul {
+public class Calcul {
 
+    /*
+     * Renvoie la somme de a et b
+     */
     public int get1(int a, int b) {
         return a + b;
     }
 
+    /*
+     * Renvoie la difference de a par b
+     */
     public int get2(int a, int b) {
         return a - b;
     }
---
+++ src/main/java/ex1/Entreprise.java
@@ -0,0 +1,18 @@
+package ex1;
+
+import java.util.Date;
+
+public class Entreprise {
+
+    private static final int CAPITAL_MAX = 3000000;
+    private int siret;
+    private String nom;
+    private String adresse;
+    private Date dateCreation;
+
+    // TODO ?
+    public void Afficher_statut() {
+
+    }
+
+}
